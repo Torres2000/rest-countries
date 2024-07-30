81ddb4f84d18df0d701f5d8bf9a7d63cec102eca
Torres2000 <pedro21t25@gmail.com>
2024-07-30 15:13:47 -0500

    Agregando los colores para el proyecto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -9,9 +11,22 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial":
+          "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "Dark-Blue":
+          "hsl(209, 23%, 22%)" /*(Dark Mode Elements)*/,
+        "Very-Dark-Blue-bg":
+          "hsl(207,26%,17%)" /* (Dark Mode Background) */,
+        "Very-Dark-Blue-text":
+          "hsl(200, 15%, 8%)" /* (Light Mode Text) */,
+        "Dark-Gray":
+          "hsl(0, 0%, 52%)" /*  (Light Mode Input) */,
+        "Very-Light-Gray":
+          "hsl(0, 0%, 98% )" /*  (Light Mode Background) */,
       },
     },
   },
