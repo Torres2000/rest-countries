bf60e69b274ee0eb3f87b76e50f99578bb77f2f3
Torres2000 <pedro21t25@gmail.com>
2024-08-06 17:35:30 -0500

    Agregando diseño responsi a las cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -9,51 +9,53 @@ export default function Cards() {
   console.log(data);
   return (
     <>
-      <section className="mt-12 flex flex-col gap-5">
-        {data.map((datos, index) => (
-          <div
-            key={index}
-            className={`max-w-[250px] w-full mx-auto rounded-lg overflow-hidden ${
-              darkMode
-                ? "bg-Dark-Blue text-white"
-                : "bg-white"
-            }`}>
-            <div className="h-[150px]">
-              <Image
-                src={datos.flag}
-                width={100}
-                height={1000}
-                alt="fsdg"
-                className="w-full h-full object-cover"
-              />
+      <section className="mt-12 ">
+        <div className="max-w-7xl mx-auto flex flex-wrap justify-between gap-5 px-5 xl:w-[90%] xl:p-0 ">
+          {data.map((datos, index) => (
+            <div
+              key={index}
+              className={`max-w-[250px] w-2/3 xl:w-full mx-auto rounded-lg overflow-hidden xl:m-0 ${
+                darkMode
+                  ? "bg-Dark-Blue text-white"
+                  : "bg-white"
+              }`}>
+              <div className="h-[150px]">
+                <Image
+                  src={datos.flag}
+                  width={1000}
+                  height={1000}
+                  alt="fsdg"
+                  className="w-full h-full"
+                />
+              </div>
+              <div className="my-5 ml-5 ">
+                <h3 className="font-bold text-lg mb-4">
+                  {datos.name}
+                </h3>
+                <ul className="space-y-1">
+                  <li className="text-sm font-light">
+                    <span className="font-medium text-sm">
+                      Population:
+                    </span>{" "}
+                    {datos.population}
+                  </li>
+                  <li className="text-sm font-light">
+                    <span className="font-medium text-sm">
+                      Region:
+                    </span>{" "}
+                    {datos.region}
+                  </li>
+                  <li className="text-sm font-light">
+                    <span className="font-medium text-sm">
+                      Capital:
+                    </span>{" "}
+                    {datos.capital}
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="my-5 ml-5 ">
-              <h3 className="font-bold text-lg mb-4">
-                {datos.name}
-              </h3>
-              <ul className="space-y-1">
-                <li>
-                  <span className="font-medium text-sm">
-                    Population:
-                  </span>{" "}
-                  {datos.population}
-                </li>
-                <li>
-                  <span className="font-medium text-sm">
-                    Region:
-                  </span>{" "}
-                  {datos.region}
-                </li>
-                <li>
-                  <span className="font-medium text-sm">
-                    Capital:
-                  </span>{" "}
-                  {datos.capital}
-                </li>
-              </ul>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
     </>
   );
