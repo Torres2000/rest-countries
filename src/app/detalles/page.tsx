c567a5fdcc6ddb0fe470d58bf1a27756c07f0d18
Torres2000 <pedro21t25@gmail.com>
2024-08-08 18:14:16 -0500

    Import de detalles en el provider

--- src/app/detalles/page.tsx
+++ src/app/detalles/page.tsx
@@ -1,5 +1,10 @@
 /** @format */
-
+import { DataProvider } from "../context";
+import Navbar from "../components/Navbar";
 export default function Detalles() {
-  return <h4>ddg</h4>;
+  return (
+    <DataProvider>
+      <Navbar />
+    </DataProvider>
+  );
 }
